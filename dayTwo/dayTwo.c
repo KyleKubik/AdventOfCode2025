6aa9988bcf48cc9045bc83aac94b8d3f3276b69c
Kyle Kubik <kyle@tocaroblue.com>
2025-12-02 22:03:25 -0600

    day two part two done

--- dayTwo/dayTwo.c
+++ dayTwo/dayTwo.c
@@ -21,6 +21,15 @@ unsigned long long int_pow(int base, int exp) {
     return result;
 }
 
+unsigned long long pattern_for_size(int digits, int pattern_size){
+  unsigned long long  pattern = 0;
+  for(long long j = digits-1; j >= 0; j-=pattern_size){
+    pattern += int_pow(10,j) ;
+  }
+  pattern = pattern/int_pow(10,pattern_size-1);
+  return pattern;
+}
+
 int main(int argc, char **argv) {
   if(argc < 2){
     printf("please specify a filename.\n");
@@ -61,20 +70,36 @@ int main(int argc, char **argv) {
     }
    
     if(calculate){
-      unsigned long long min_order = (floor(log10(min_val))+1)/2;
-      unsigned long long max_order = (floor(log10(max_val))+1)/2;
+      unsigned long long min_digits = floor(log10(min_val))+1;
+      unsigned long long max_digits = floor(log10(max_val))+1;
       printf("values: %llu - %llu\n",min_val,max_val);
-      printf("orders: %llu - %llu\n",min_order,max_order);
+      printf("  digits: %llu - %llu\n",min_digits,max_digits);
       
-      for(long long i = min_order; i <= max_order; i++){
-        printf("   order: %llu\n", i );
-        unsigned long long  pattern = int_pow(10,i)+1 ;
-        unsigned long long  min_possible = int_pow(10,i-1);
-        unsigned long long  max_possible = int_pow(10,i)-1;
-        for(unsigned long long j = min_possible; j <= max_possible; j++){
-          if(min_val <= j*pattern && j*pattern <= max_val){
-            printf("bad value: %llu\n", j*pattern );
-            total += j*pattern;
+      for(int digits = min_digits; digits <= max_digits; digits++){
+        for(int pattern_size = 1; pattern_size <= digits/2; pattern_size++){
+          if(digits % pattern_size == 0){
+            unsigned long long pattern = pattern_for_size(digits, pattern_size);
+            unsigned long long  min_possible = int_pow(10,pattern_size-1);
+            unsigned long long  max_possible = int_pow(10,pattern_size)-1;
+            printf("    pattern: %llu (size %llu)\n", pattern, pattern_size );
+            for(unsigned long long j = min_possible; j <= max_possible; j++){
+              if(min_val <= j*pattern && j*pattern <= max_val){
+                unsigned long long value = j*pattern;
+                bool already_seen = false;
+                for(int k = pattern_size-1; k >= 1; k--){
+                  unsigned long long pre_pattern = pattern_for_size(digits, k);
+                  if(value % pre_pattern == 0){
+                    printf("      * ALREADY SEEN: %llu\n", value );
+                    already_seen = true;
+                    break;
+                  }
+                }
+                if(!already_seen){
+                  printf("      * bad value: %llu\n", value );
+                  total += value;
+                }
+              }
+            }
           }
         }
       }
